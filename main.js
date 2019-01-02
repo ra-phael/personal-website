3eebc4d6aa73eda333753f0411e2dbf172dd4151
Raphael <rapssail@hotmail.fr>
2019-01-02 18:09:24 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+console.log('Looking for something??');
+
+function onMenuItemClick() {
+    if(window.screen.width <= 420) { 
+        toggleMenu();
+    }
+
+}
+
+function toggleMenu() {
+    var header = document.getElementById("header");
+    var mobileMenuIcon = document.getElementById("mobile-menu-icon");
+
+    header.classList.toggle("menu-revealed")
+    mobileMenuIcon.classList.toggle("mobile-menu__icon--opened");
+}
